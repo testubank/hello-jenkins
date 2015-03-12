2591766cd05f44763914f4f1161c70e3ba20c051
UBank <ubank@orchardserver.aur.national.com.au>
2015-03-12 11:38:44 +1100

    Switch to hello jenkins

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express = require('express');
+8var express = require('express');
  
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.send('hello jenkins');
 });
  
 app.listen(process.env.PORT || 5000);
