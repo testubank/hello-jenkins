338cecc6d10b4b2881c4a813a5441c8686d2b113
UBank <ubank@orchardserver.aur.national.com.au>
2015-03-13 09:48:46 +1100

    Fixing app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-8var express = require('express');
+var express = require('express');
  
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello wjenkins');
+  res.send('hello world');
 });
  
 app.listen(process.env.PORT || 5000);
